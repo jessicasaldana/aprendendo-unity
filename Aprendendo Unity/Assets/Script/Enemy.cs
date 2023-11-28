1db2e51fcaa21f261a29f24e1a52bd7d1b541ba4
Jessica Saldaña <jessicarsaldana@hotmail.com>
2023-11-28 15:51:24 -0300

    Iniciando projeto
    Iniciando projeto Unity
    Criando script Player e Enemy

---
+++ Aprendendo Unity/Assets/Script/Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    void Start() // Start is called before the first frame update
+    {
+        
+    }
+    
+    void Update() // Update is called once per frame
+    {
+        
+    }
+
+    void Movement() //mover
+    {
+
+    }
+
+    void SeekPlayer() //procurar o player
+    {
+
+    }
+
+    void FollowPlayer() // seguir o player
+    {
+
+    }
+
+    void Attack() //atacar
+    {
+
+    }
+}
---
+++ Aprendendo Unity/Assets/Script/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    void Start() // Start is called before the first frame update
+    {
+        
+    }
+
+    void Update() // Update is called once per frame
+    {
+        
+    }
+
+    void Movement() //mover
+    {
+
+    }
+
+    void Jump() //pular
+    {
+
+    }
+
+    void Attack() //atacar
+    {
+
+    }
+}
