b775d015fd0e4baae46cd50f13663043123dd53b
Jessica Saldaña <jessicarsaldana@hotmail.com>
2024-01-16 17:55:05 -0300

    Criando animação e movimentação do player

--- Aprendendo Unity/Assets/Script/Player.cs
+++ Aprendendo Unity/Assets/Script/Player.cs
@@ -7,14 +7,28 @@ public class Player : MonoBehaviour
     public int health;
     public float speed;
 
+    private Rigidbody2D rig;
+    private Vector2 _direction;
+
+    public Vector2 direction
+    {
+        get { return direction; }
+        set { _direction = value; }
+    }
+   
     void Start() // Start is called before the first frame update
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     void Update() // Update is called once per frame
     {
-        
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
     }
 
     void Movement() //mover
