82f7815312e6f02fb27aa9479476a085625abdd8
Jessica Saldaña <jessicarsaldana@hotmail.com>
2024-01-17 21:45:01 -0300

    add corrida ao player

--- Aprendendo Unity/Assets/Script/Player.cs
+++ Aprendendo Unity/Assets/Script/Player.cs
@@ -4,45 +4,72 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int health;
-    public float speed;
+    [SerializeField] private int health;
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
 
     private Rigidbody2D rig;
+
+    private float initialSpeed;
+    private bool _isRunning;
     private Vector2 _direction;
 
     public Vector2 direction
     {
-        get { return direction; }
+        get { return _direction; }
         set { _direction = value; }
     }
-   
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
     void Start() // Start is called before the first frame update
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
     }
 
     void Update() // Update is called once per frame
     {
-        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnInput();
+        OnRun();
     }
 
     private void FixedUpdate()
     {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
         rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
     }
 
-    void Movement() //mover
+    void OnRun()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
 
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
     }
 
-    void Jump() //pular
-    {
+    #endregion
 
-    }
-
-    void Attack() //atacar
-    {
-
-    }
 }
